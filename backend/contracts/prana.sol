20eab9a301852b88c01ab7fd9d9cf8317c54ea08
richuak <richuak@gitlab.com>
2020-07-11 20:34:27 +0530

    publishBook( ) added

--- backend/contracts/prana.sol
+++ backend/contracts/prana.sol
@@ -26,14 +26,14 @@ abstract contract prana is ERC1155 {
     }
 
     // struct to store book details. For each new title.
-    // bytes32(encryptedBookData) - the actual content of the book
+    // bytes32(encryptedBookDataHash) - the actual content of the book
     // This is where the linkage of the contract with storage mechanisms happen
     // address(publisherAddress) -  address of the content creator/publisher
     // uint256(bookPrice) - price of the book that the creator asks for direct purchase
     // uint256(transactionCut) - cut of further transactions on copies 
     // that the creator lay claim to. Stored as a percentage.
     struct BookInfo{
-        bytes32 encryptedBookData;  
+        bytes32 encryptedBookDataHash;  
         address publisherAddress;
         uint256 bookPrice;   
         uint256 transactionCut;    
@@ -46,6 +46,25 @@ abstract contract prana is ERC1155 {
 
     //Event to emit when a new book is published with its ISBN and publisher address
     event BookPublished(address indexed publisher, uint256 indexed isbn, uint256 indexed price);
+
+
+    //function to add book details into the chain i.e. publish the book
+    function publishBook(
+        bytes32 _encryptedBookDataHash, 
+        uint256 _isbn, 
+        uint256 _price, 
+        uint256 _transactionCut) 
+        public {
+        require(booksInfo[_isbn].publisherAddress==address(0), "This book has already been published!");
+        booksInfo[_isbn].encryptedBookDataHash = _encryptedBookDataHash;
+        booksInfo[_isbn].publisherAddress = tx.origin;
+        booksInfo[_isbn].bookPrice = _price;
+        booksInfo[_isbn].transactionCut = _transactionCut;
+        
+        //event that serves as an advertisement, sort of.
+        emit BookPublished(tx.origin, _isbn, _price);
+
+    }
 
 
 
