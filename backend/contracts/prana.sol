021c044af8b8ee4888b9fecb550dfe8b518b1d62
richuak <richuak@gitlab.com>
2020-07-11 19:49:16 +0530

    BookInfo struct & corresponding mapping

--- backend/contracts/prana.sol
+++ backend/contracts/prana.sol
@@ -18,4 +18,32 @@ abstract contract prana is ERC1155 {
     //address of the contract deployer
     address owner;
 
+    //Modifier for onlyOwner functions
+    modifier onlyOwner {
+       require(msg.sender == owner, 'You are not the contract owner to call this function!');
+       _;
+    }
+
+    // struct to store book details. For each new title.
+    // bytes32(encryptedBookData) - the actual content of the book
+    // This is where the linkage of the contract with storage mechanisms happen
+    // address(publisherAddress) -  address of the content creator/publisher
+    // uint256(bookPrice) - price of the book that the creator asks for direct purchase
+    // uint256(transactionCut) - cut of further transactions on copies 
+    // that the creator lay claim to. Stored as a percentage.
+    struct BookInfo{
+        bytes32 encryptedBookData;  
+        address publisherAddress;
+        uint256 bookPrice;   
+        uint256 transactionCut;    
+    }
+
+    // mapping for all books
+    // ISBN is the key, its corresponding details is the value
+    mapping(uint256 => BookInfo) internal booksInfo;
+
+
+
+
+
 }
