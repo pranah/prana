6ef33b4378ec0132861694e01ee7d48d8088f7e9
richuak <richuak@gitlab.com>
2020-07-12 10:45:36 +0530

    comment edit

--- backend/contracts/prana.sol
+++ backend/contracts/prana.sol
@@ -91,7 +91,7 @@ abstract contract prana is ERC1155 {
         booksInfo[_isbn].transactionCut = _transactionCut;
         booksInfo[_isbn].bookSales = 0;
         
-        //event that serves as an advertisement, sort of.
+        //event that serves as an advertisement as well
         emit BookPublished(tx.origin, _isbn, _price);
 
     }
